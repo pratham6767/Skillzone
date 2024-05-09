55d3f3bed7d40f478ca9fb3268eb97304500a003
pratham6767 <prathampatel8316@gmail.com>
2024-05-09 20:15:57 +0530

    Added deletion controller

--- server/controllers/Profile.js
+++ server/controllers/Profile.js
@@ -59,3 +59,40 @@ exports.updateProfile = async (req, res) => {
   }
 }
 
+exports.deleteAccount = async (req, res) => {
+    try {
+      const id = req.user.id
+      console.log(id)
+      const user = await User.findById({ _id: id })
+      if (!user) {
+        return res.status(404).json({
+          success: false,
+          message: "User not found",
+        })
+      }
+      // Delete Assosiated Profile with the User
+      await Profile.findByIdAndDelete({
+        _id: new mongoose.Types.ObjectId(user.additionalDetails),
+      })
+      for (const courseId of user.courses) {
+        await Course.findByIdAndUpdate(
+          courseId,
+          { $pull: { studentsEnroled: id } },
+          { new: true }
+        )
+      }
+      // Now Delete User
+      await User.findByIdAndDelete({ _id: id })
+      res.status(200).json({
+        success: true,
+        message: "User deleted successfully",
+      })
+      await CourseProgress.deleteMany({ userId: id })
+    } catch (error) {
+      console.log(error)
+      res
+        .status(500)
+        .json({ success: false, message: "User Cannot be deleted successfully" })
+    }
+  }
+  
