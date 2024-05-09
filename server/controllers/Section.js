42b4ac88c6e9b3d1b8ae08a837602984df30470e
pratham6767 <prathampatel8316@gmail.com>
2024-05-09 18:52:04 +0530

    Added Deletion of Section

--- server/controllers/Section.js
+++ server/controllers/Section.js
@@ -81,4 +81,53 @@ exports.updateSection = async (req, res) => {
     catch(error){
 
     }
+};
+
+
+// DELETE a section
+exports.deleteSection = async (req, res) => {
+	try {
+
+        const { sectionId, courseId }  = req.body;
+		await Course.findByIdAndUpdate(courseId, {
+			$pull: {
+				courseContent: sectionId,
+			}
+		})
+		const section = await Section.findById(sectionId);
+		console.log(sectionId, courseId);
+		if(!section) {
+			return res.status(404).json({
+				success:false,
+				message:"Section not Found",
+			})
+		}
+
+		//delete sub section
+		await SubSection.deleteMany({_id: {$in: section.subSection}});
+
+		await Section.findByIdAndDelete(sectionId);
+
+		//find the updated course and return 
+		const course = await Course.findById(courseId).populate({
+			path:"courseContent",
+			populate: {
+				path: "subSection"
+			}
+		})
+		.exec();
+
+		res.status(200).json({
+			success:true,
+			message:"Section deleted",
+			data:course
+		});
+
+    }catch(error){
+        console.error("Error deleting section:", error);
+		res.status(500).json({
+			success: false,
+			message: "Internal server error",
+		});
+    }
 }
