4f84a97fe920020904fc9fc055b3029d1bf1e251
pratham6767 <prathampatel8316@gmail.com>
2024-05-10 20:02:58 +0530

    Rzorpay config

---
+++ server/controllers/Payments.js
@@ -0,0 +1,11 @@
+const { instance } = require("../config/razorpay")
+const Course = require("../models/Course")
+const crypto = require("crypto")
+const User = require("../models/User")
+const mailSender = require("../utils/mailSender")
+const mongoose = require("mongoose")
+const {
+  courseEnrollmentEmail,
+} = require("../mail/templates/courseEnrollmentEmail")
+const { paymentSuccessEmail } = require("../mail/templates/paymentSuccessEmail")
+const CourseProgress = require("../models/CourseProgress")
